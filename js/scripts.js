d0316be847ad69cbc4677e31c7fb96fee908bd4f
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-17 14:45:21 -0700

    Keep running total from die roll and add to total display, added individual roll and hold button for players
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,73 @@
 //Business Logic
 function Player (player1, player2, game){
-  this.players = [player1, player2];
+  this.player1 = player1;
+  this.player2 = player2;
   this.game = game;
+  this.currentPlayer = "player1";
 }
 
-function Game (turnScore, total, gameWins, gameLoses) {
-  this.turnScore = 100;
+function Game (turnScore, totalScore, gameWins, gameLoses) {
+  this.turnScore = 0;
   this.totalScore = 0;
   this.gameWins = 0;
   this.gameLoses = 0;
 }
 
-Player.prototype.switchUser = function() {
+function SwitchUser () {
   this.turnScore = 0; // clear turn points when players switch
-  if (this.players === [0]) { // checks to see if current player is at index 0 (player1)
-    this.players = [1]; // makes player2 current player
+  if (this.currentPlayer === "player1") { // checks to see if current player is at index 0 (player1)
+    this.currentPlayer = "player2"; // makes player2 current player
   } else {
-    this.players = [0]; // makes player 1 current player
+    this.currentPlayer = "player1"; // makes player 1 current player
   }
 }
 
 Game.prototype.roll = function() {
   let newRoll = Math.floor((Math.random() * 6) + 1);
-  if (newRoll !== 1) {
-    this.turnScore += newRoll;
-  } else if (totalScore += turnScore >= 100) {
-    alert("You won!");
-  } else {
+  console.log("your roll:" + newRoll);
+  if (newRoll === 1) {
+    this.turnScore = 0;
     alert("You rolled a 1! Your score for this round is 0, and your turn is over!");
-    Player.switchUser();
-     // changes players
+    // SwitchUser(); // changes players
+  } else if (newRoll != 1) {
+    this.turnScore += newRoll;
+  } else {
+    this.totalScore += newRoll >= 100;
+    alert("You won!");
+    
   }
-  return newRoll;
+  return this.totalScore;
 }
 
 Game.prototype.hold = function() {
-  return this.turnScore += this.totalScore
+  this.totalScore += this.turnScore;
   alert("Your turn is over pass the mouse!");
-  Player.switchUser(); // changes players
+  this.turnScore = 0;
+  // SwitchUser(); // changes players
+  console.log("your total score:" + this.totalScore);
 }
 
 //UI Logic
 $(document).ready(function(){
-  $("#roll").click(function() {
-    
-  $("#hold").click(function() {
+  let playerOne = new Game("player1");
+  let playerTwo = new Game("player2");
+  $("#roll-player-one").click(function() {
+    playerOne.roll();
+    // SwitchUser();
+    $(".p1-score").text(" " + playerOne.turnScore); 
+  })
+    $("#hold-player-one").click(function() {
+      playerOne.hold();
+      $(".p1-total").text(" " + playerOne.totalScore);
+  })
+
+  $("#roll-player-two").click(function() {
+    playerTwo.roll();
+    $(".p2-score").text(" " + playerTwo.turnScore);
+  })
+  $("#hold-player-two").click(function() {
+    playerTwo.hold();
+    $(".p2-total").text(" " + playerTwo.totalScore);
     
   })
-  })
-})
+})    
