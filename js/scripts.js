81c56831dac1a1add5cb2264fe6cf1c1983945fa
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-17 15:44:40 -0700

    Add additional styles to stylesheet and html structure
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-//Business Logic
+//Business Logic -----
+
+// Backend logic for Game
 function Player (player1, player2, game){
   this.player1 = player1;
   this.player2 = player2;
@@ -13,6 +15,7 @@ function Game (turnScore, totalScore, gameWins, gameLoses) {
   this.gameLoses = 0;
 }
 
+// Switch player turn function (needs work)
 function SwitchUser () {
   this.turnScore = 0; // clear turn points when players switch
   if (this.currentPlayer === "player1") { // checks to see if current player is at index 0 (player1)
@@ -22,25 +25,23 @@ function SwitchUser () {
   }
 }
 
+// Roll button method, outputs a random number
 Game.prototype.roll = function() {
   let newRoll = Math.floor((Math.random() * 6) + 1);
   console.log("your roll:" + newRoll);
-  if (newRoll === 1) {
-    this.turnScore = 0;
-    alert("You rolled a 1! Your score for this round is 0, and your turn is over!");
-    // SwitchUser(); // changes players
-  } else if (newRoll != 1) {
-    this.turnScore += newRoll;
-  } else {
-    this.totalScore += newRoll >= 100;
-    alert("You won!");
-    
+  if (newRoll != 1) {
+    this.turnScore += newRoll; // pushes new roll into turnScore
+    if (this.totalScore + this.turnScore >= 100) {
+      alert("You won!");
+    }
+  } else { // If user rolls one you will get this alert
+    alert("You rolled a 1! Your score for this round is 0, and your turn is over!"); 
   }
   return this.totalScore;
 }
 
 Game.prototype.hold = function() {
-  this.totalScore += this.turnScore;
+  this.totalScore += this.turnScore; // add turnScore to players totalScore
   alert("Your turn is over pass the mouse!");
   this.turnScore = 0;
   // SwitchUser(); // changes players
@@ -56,11 +57,10 @@ $(document).ready(function(){
     // SwitchUser();
     $(".p1-score").text(" " + playerOne.turnScore); 
   })
-    $("#hold-player-one").click(function() {
-      playerOne.hold();
-      $(".p1-total").text(" " + playerOne.totalScore);
+  $("#hold-player-one").click(function() {
+    playerOne.hold();
+    $(".p1-total").text(" " + playerOne.totalScore);
   })
-
   $("#roll-player-two").click(function() {
     playerTwo.roll();
     $(".p2-score").text(" " + playerTwo.turnScore);
@@ -68,6 +68,5 @@ $(document).ready(function(){
   $("#hold-player-two").click(function() {
     playerTwo.hold();
     $(".p2-total").text(" " + playerTwo.totalScore);
-    
   })
 })    
