93baba7db1aca54c47e34ed64671d2dd458d9907
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-18 11:47:34 -0700

    Add win counter and reset total/turnscore button
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 //Business Logic -----
 
 // Backend logic for Game
-function Player (player1, player2, game){
+function Player (player1, player2){
   this.player1 = player1;
   this.player2 = player2;
-  this.game = game;
   this.currentPlayer = "player1";
 }
 
@@ -12,7 +11,12 @@ function Game () {
   this.turnScore = 0;
   this.totalScore = 0;
   this.gameWins = 0; // add functionality
-  this.gameLoses = 0; // add functionality 
+}
+
+function Winner() {{
+  this.gameWins += 1; 
+  }
+  return this.gameWins;
 }
 
 // Switch player turn function (needs work)
@@ -20,12 +24,12 @@ function SwitchUser () {
   this.turnScore = 0; // clear turn points when players switch
   if (this.currentPlayer === "player2") { // checks to see if current player is at index 0 (player1)
     this.currentPlayer = "player1"; // makes player2 current player
-    $(".card-body1").show();
-    $(".card-body2").hide();
+    $(".card-body1").fadeIn();
+    $(".card-body2").fadeOut();
     } else {
       this.currentPlayer = "player2"; // makes player 1 current player
-      $(".card-body2").show();
-      $(".card-body1").hide();
+      $(".card-body2").fadeIn();
+      $(".card-body1").fadeOut();
   }
 }
 
@@ -38,7 +42,8 @@ Game.prototype.roll = function() {
   diceStatus.innerHTML = newRoll;
   if (newRoll != 1) {
     this.turnScore += newRoll; // pushes new roll into turnScore
-    if (this.totalScore + this.turnScore >= 100) {
+    if (this.totalScore + this.turnScore >= 10) {
+      Winner();
       alert("You won!");
     }
   } else { // If user rolls one you will get this alert
@@ -58,11 +63,21 @@ Game.prototype.hold = function() {
 
 //UI Logic
 $(document).ready(function(){
+  $("form#player-names").submit(function(event){
+    event.preventDefault();
+    let namePlayer1 = $("input#player-1-name").val();
+    let namePlayer2 = $("input#player-2-name").val();
+    $("#p1name").text(namePlayer1);
+    $("#p2name").text(namePlayer2);
+    $("#pig-dice").fadeIn();
+    $("#player-names").fadeOut();
+  })
   let playerOne = new Game("player1");
   let playerTwo = new Game("player2");
   $("#roll-player-one").click(function() {
     playerOne.roll();
-    $(".p1-score").text(" " + playerOne.turnScore); 
+    $(".p1-score").text(" " + playerOne.turnScore);
+    $(".p1-win").text(" " + playerOne.gameWins);
   })
   $("#hold-player-one").click(function() {
     playerOne.hold();
@@ -71,9 +86,13 @@ $(document).ready(function(){
   $("#roll-player-two").click(function() {
     playerTwo.roll();
     $(".p2-score").text(" " + playerTwo.turnScore);
+    $(".p2-win").text(" " + playerTwo.gameWins);
   })
   $("#hold-player-two").click(function() {
     playerTwo.hold();
     $(".p2-total").text(" " + playerTwo.totalScore);
+  })
+  $("#new-game").click(function() {
+    $(".p2-score", ".p2-total", ".p1-score", ".p1-total").reset("0");
   })
 });    
